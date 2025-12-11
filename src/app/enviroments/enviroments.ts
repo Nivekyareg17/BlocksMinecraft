344a29eda985f213d23c47581c719112eb633575
Nivekyareg17 <guevaraclavijok@gmail.com>
2025-12-11 08:46:35 -0500

    update 1.3

--- src/app/enviroments/enviroments.ts
+++ src/app/enviroments/enviroments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://crudcrud.com/api/5bd8787564d348f3a070afdc45f517f7'
+  apiBaseUrl: 'https://crudcrud.com/api/d91b3fce16154160987ae6a02bad0320'
 };
